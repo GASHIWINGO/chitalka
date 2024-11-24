a53fb01c4284f407321f7bfdc86fbfc5c502b0f0
GASHIWINGO <117731094+GASHIWINGO@users.noreply.github.com>
2024-11-24 22:39:00 +0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 
+// Основной компонент для настройки маршрутов и отображения заголовков
 export default function Layout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
